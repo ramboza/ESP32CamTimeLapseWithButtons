e823cd0d9a57c9ed6346cbdf9b512745e38f5d7c
Jenya <jenya@jenya.org>
2022-05-15 03:45:18 +0200

    running without wifi

--- TimeLapse/TimeLapse.ino
+++ TimeLapse/TimeLapse.ino
@@ -5,10 +5,11 @@
 #include "lapse.h"
 #include "photo.h"
 
-const char *ssid = "...";
-const char *password = "........";
+const char *ssid = "WIFI_SSID";
+const char *password = "CHANGE_IT_ON_YOUR_WIFIPASSWORD";
 const int PIN_BUTTON_TX = 1;
 const int PIN_BUTTON_RX = 3;
+int wifi_attempts = 15;
 
 void startCameraServer();
 
@@ -16,21 +17,26 @@ void setup() {
         Serial.begin(115200);
         Serial.setDebugOutput(true);
         Serial.println();
+
         initFileSystem();
         initCamera();
 
         WiFi.begin(ssid, password);
-        while (WiFi.status() != WL_CONNECTED) {
-                delay(500);
-                Serial.print(".");
-        }
-        Serial.println("");
-        Serial.println("WiFi connected");
-        startCameraServer();
-        Serial.print("Camera Ready! Use 'http://");
-        Serial.print(WiFi.localIP());
-        Serial.println("' to connect");
 
+	// no wifi, no problem
+	Serial.println("\nConnecting to Wi-Fi...\n");
+        for (int i = 1; i < wifi_attempts; i++) {
+		if (WiFi.status() != WL_CONNECTED) {
+                	delay(500);
+                	Serial.printf("retry %d of %d\n", i, wifi_attempts);
+		} else {
+			startCameraServer();
+        		Serial.printf("\nCamera Ready!\n\nUse 'http://%s' to connect\n", WiFi.localIP().toString());
+			break;
+		}
+	}
+
+       	Serial.println("\nReconfiguring UART RX/TX as Photo/TimeLapse buttons...\ndisconnected");
         Serial.end();
         pinMode(PIN_BUTTON_TX, INPUT_PULLUP);
         pinMode(PIN_BUTTON_RX, INPUT_PULLUP);
@@ -88,7 +94,7 @@ void button_RX_Photo(void) {
 
 void blinkN(int times) {
 	for (int i = 0; i < times; i++){
-		digitalWrite(33, LOW); 
+		digitalWrite(33, LOW);
 		delay(333);
 		digitalWrite(33, HIGH);
 		delay(333);
